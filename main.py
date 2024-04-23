ba86a640c0a3c56b6d6e677616461e41be82078a
Iwni <ir1c1719@gmail.com>
2024-04-23 20:17:07 +0300

    Made LoginForm and Login. Later: fix the username display.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request, redirect
-import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
+
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField, BooleanField
+from wtforms.validators import DataRequired
+from flask_login import LoginManager, login_user
+
+import sqlalchemy
 from sqlalchemy import orm
 from sqlalchemy.orm import Session
 
-ADMIN_KEY = '123abc456def'
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+ADMIN_KEY = '123abc456def'  # !!!!!!
 
 
 SqlAlchemyBase = orm.declarative_base()
@@ -14,10 +23,12 @@ __factory = None
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'  # !!!!!!
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
-
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 def global_init(db_file):
     global __factory
@@ -26,7 +37,7 @@ def global_init(db_file):
         return
 
     if not db_file or not db_file.strip():
-        raise Exception("Необходимо указать файл базы данных.")
+        raise Exception('Необходимо указать файл базы данных.')
 
     conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
 
@@ -39,6 +50,15 @@ def global_init(db_file):
 def create_session() -> Session:
     global __factory
     return __factory()
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    secret_key = TextAreaField('Секретный ключ (для администраторов)')
+    submit = SubmitField('Войти')
 
 
 class Article(SqlAlchemyBase):
@@ -54,27 +74,46 @@ class Article(SqlAlchemyBase):
         return '<Article %r>' % self.id
 
 
+
+
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    nickname = sqlalchemy.Column(sqlalchemy.String(20), nullable=False, unique=True)
-    password = sqlalchemy.Column(sqlalchemy.String(20), nullable=False)
+    name = sqlalchemy.Column(sqlalchemy.String(20), nullable=False, unique=True)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now(timezone.utc))
     is_admin = sqlalchemy.Column(sqlalchemy.Boolean, nullable=False)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
 
     def __repr__(self):
         return '<User %r>' % self.id
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    email = EmailField('Почта', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = create_session()
+    return db_sess.query(User).get(user_id)
+
+
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template("index.html")
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
+    return render_template('index.html')
 
 
 @app.route('/FAQ')
@@ -84,11 +123,12 @@ def faq():
 
 @app.route('/for_admins')
 def for_admins():
-    return render_template("for_admins.html")
+    return render_template('for_admins.html')
 
-@app.route('/create-article', methods=['POST', "GET"])
+
+@app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
-    if request.method == "POST":
+    if request.method == 'POST':
         title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
@@ -101,33 +141,47 @@ def create_article():
             return redirect('/')
 
         except:
-            return "При добавлении статьи произошла ошибка"
+            return 'При добавлении статьи произошла ошибка'
     else:
-        return render_template("create-article.html")
+        return render_template('create-article.html')
 
 
-@app.route('/sign_up', methods=['POST', "GET"])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
 def register():
-    if request.method == "POST":
-        nickname = request.form['nickname']
-        password = request.form['password']
-        key = request.form['key']
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message='Пароли не совпадают')
+        if db.session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message='Такой пользователь уже есть')
+        is_admin = True if form.secret_key.data == ADMIN_KEY else False
+        user = User(name=form.name.data, email=form.email.data, is_admin=is_admin)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/login')
+    return render_template('register.html', form=form)
 
-        is_admin = True if key == ADMIN_KEY else False
 
-        user = User(nickname=nickname, password=password, is_admin=is_admin)
-
-        try:
-            db.session.add(user)
-            db.session.commit()
-            return redirect('/')
-
-        except:
-            return "При регистрации произошла ошибка"
-    else:
-        return render_template("register.html")
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     global_init('instance/main.db')
     app.run(debug=True)
